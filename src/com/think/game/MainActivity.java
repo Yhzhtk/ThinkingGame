2a44681ff0d89372a573a1084092715c58e023a5
gudh <yhzhtk@gmail.com>
2013-11-11 11:25:10 +0800

    添加libgdx游戏开发框架

--- src/com/think/game/MainActivity.java
+++ src/com/think/game/MainActivity.java
@@ -1,23 +1,65 @@
 package com.think.game;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends AndroidApplication {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initialize(new FGame(), false);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
+
+class FGame implements ApplicationListener {
+	// 绘图用的SpriteBatch
+	private SpriteBatch batch;
+	// 纹理
+	private Texture texture;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch(); // 实例化
+		texture = new Texture(Gdx.files.internal("p.png"));
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT); // 清屏
+		batch.begin();
+		batch.draw(texture, 20, 10);
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void resume() {
+	}
 }
