b9c704fd51b8a8f1ba891df2c0ee9089321488da
gudh <yhzhtk@gmail.com>
2013-11-12 18:35:18 +0800

    已完成FGame游戏基本功能，可以玩游戏比得分 1、添加FGameAct作为游戏主类，实现全局控制 2、添加FGameInputProcess作为事件处理类 3、添加FGameParameter作为存储参数类 4、添加uiskin和default相关的资源文件

---
+++ src/com/think/game/FGameInputProcessor.java
@@ -0,0 +1,121 @@
+package com.think.game;
+
+import android.util.Log;
+
+import com.badlogic.gdx.InputProcessor;
+
+/**
+ * FGame事件处理
+ * @author gudh
+ * @date 2013-11-12
+ */
+public class FGameInputProcessor implements InputProcessor {
+
+	public FGameAct fGame;
+
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	private int pw;
+	private int ph;
+
+	private int height;
+
+	public FGameInputProcessor(FGameAct fGame) {
+		this.fGame = fGame;
+		FGameParameter para = fGame.para;
+		x1 = (int) para.getGameBound().getX();
+		y1 = (int) para.getGameBound().getY();
+		x2 = (int) para.getGameBound().getWidth() + x1;
+		y2 = (int) para.getGameBound().getHeight() + y1;
+		pw = para.getRectSize()[0];
+		ph = para.getRectSize()[1];
+
+		height = para.getScreenHeight();
+	}
+
+	/**
+	 * 响应TouchUp事件
+	 * 
+	 * @param arg0
+	 * @param arg1
+	 * @param arg2
+	 * @param arg3
+	 * @return
+	 */
+	public boolean actEventTouchUp(int arg0, int arg1, int arg2, int arg3) {
+		// y轴转换
+		arg1 = height - arg1;
+		Log.d("Event", "转换后的坐标:" + arg0 + " " + arg1);
+
+		if (arg0 < x1 || arg0 > x2 || arg1 < y1 || arg1 > y2) {
+			Log.i("Event", "不在游戏区");
+			// 处理开始按钮
+
+		} else {
+			int x = (arg0 - x1) / pw;
+			int y = (arg1 - y1) / ph;
+			Log.i("Event", "点击 ： " + x + " " + y);
+			fGame.playOnce(x, y);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+		String str = "Up " + arg0 + " " + arg1 + " " + arg2 + " " + arg3;
+		Log.d("Event", str);
+		return actEventTouchUp(arg0, arg1, arg2, arg3);
+	}
+
+	@Override
+	public boolean touchDragged(int arg0, int arg1, int arg2) {
+		String str = "Drag " + arg0 + " " + arg1 + " " + arg2;
+		Log.d("Event", str);
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
+		String str = "Down " + arg0 + " " + arg1 + " " + arg2 + " " + arg3;
+		Log.d("Event", str);
+		return false;
+	}
+
+	@Override
+	public boolean keyDown(int arg0) {
+		String str = "KeyDown " + arg0;
+		Log.d("Event", str);
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char arg0) {
+		String str = "KeyTyped " + arg0;
+		Log.d("Event", str);
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int arg0) {
+		String str = "KeyUp " + arg0;
+		Log.d("Event", str);
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int arg0, int arg1) {
+		String str = "MouseMove " + arg0 + " " + arg1;
+		Log.d("Event", str);
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int arg0) {
+		String str = "Scrolled " + arg0;
+		Log.d("Event", str);
+		return false;
+	}
+}
--- src/com/think/game/MainActivity.java
+++ src/com/think/game/MainActivity.java
@@ -3,15 +3,7 @@ package com.think.game;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.think.game.f.FGameUtil;
 
 public class MainActivity extends AndroidApplication {
 
@@ -19,7 +11,9 @@ public class MainActivity extends AndroidApplication {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		initialize(new FGame(), false);
+
+		// 初始化游戏
+		initialize(new FGameAct(), false);
 	}
 
 	@Override
@@ -27,58 +21,5 @@ public class MainActivity extends AndroidApplication {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-}
-
-class FGame implements ApplicationListener {
-
-	// 存储所有颜色对象
-	static Texture[] textures;
-
-	// 绘图用的SpriteBatch
-	private SpriteBatch batch;
-
-	@Override
-	public void create() {
-		batch = new SpriteBatch(); // 实例化
-
-		int length = FGameUtil.getAllColors().length;
-		textures = new Texture[length];
-
-		for (int i = 0; i < length; i++) {
-			Pixmap p = new Pixmap(2, 2, Format.RGBA8888);
-			p.setColor(FGameUtil.getAllColors()[i]);
-			p.fillRectangle(0, 0, 2, 2);
-			textures[i] = new Texture(p, false);
-		}
-	}
-
-	@Override
-	public void dispose() {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1); //背景色
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(textures[0], 0, 0, 20, 20);
-		batch.draw(textures[1], 40, 20, 20, 20);
-		batch.draw(textures[2], 20, 40, 20, 20);
-		batch.draw(textures[3], 40, 60, 20, 20);
-		batch.draw(textures[4], 60, 40, 20, 20);
-		batch.end();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void resume() {
 	}
 }
